8f28108d513fdb1a0188222e778e00518852b0b8
Ben Moes <ben.moes@icloud.com>
2013-02-02 22:11:34 +0100

    Initial Commit

---
+++ Matchismo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Matchismo
+//
+//  Created by Ben Moes on 02-02-13.
+//  Copyright (c) 2013 Ben Moes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
